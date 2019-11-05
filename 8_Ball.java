01922fe04cec11713318c107143c7f637165728a
b <49073956+okanobr@users.noreply.github.com>
2019-11-05 22:35:20 +0800

    Create 8_Ball.java

---
+++ 8_Ball.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class 8_Ball 
+{
+	public static void main(String[] args) 
+  {
+		Scanner input = new Scanner(System.in);
+    
+    while(true)
+    {
+      System.out.print("Ask me a question! (Use correct English grammar for useful results)");
+      String question = input.nextLine().toLowerCase();
+      
+      if(question.startsWith("what is")
+      {
+      System.out.print("It is" + question.substring(7) + ".");
+      }
+    }
+	}
+}
