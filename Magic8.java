b072904e08fa27f87764138d24287f7d3b5c28d1
brendon <49073956+okanobr@users.noreply.github.com>
2019-11-17 19:38:08 +0800

    Revert "[unstable ]not a question parameters dont work but im giving up for now"
    This reverts commit 3a2e9fa46752b3a68cc9909d56394ec1477f58d8.

--- Magic8.java
+++ Magic8.java
@@ -1,140 +1,72 @@
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 public class Magic8 
 {
-    public static String getLastChar(String s) 
-    {
-        return s.substring(s.length() - 1); 
-    }
-
-    public static int findIndex(String[] arr, String t) // method i copy pasted lol
-    { 
-        int index = -1;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i].equals(t)) {
-                index = i;
-                break;
-            }
-        }
-        return index;
-    } 
-
-    public static boolean isSensical(String question) // checks if question is nonsense, returns boolean
-    {
-        // i built this method initially but after lots of trial and error i dont understand how it even works
-        String[] qWords = question.split(" ");
-        boolean booFin = true;
-
-        String[] interro = {"who", "what", "when", "where", "why", "how"};
-        int interroCount = 0;        
-        
-        for(String inteWord : interro) {
-            for(int i = 0; i < qWords.length; i++) {
-                if(qWords.equals(inteWord))
-                    interroCount++;
-                    break;
-            }
-        }
-
-        if(interroCount > 1)
-            booFin = false;
-        else
-            booFin = true;
-
-        return booFin; 
-    }
-
-    public static String getCurrentTime() 
-    {
-        Date date = new Date();
-        String strDateFormat = "hh:mm a.";
-        DateFormat dateFormat = new SimpleDateFormat(strDateFormat);
-        String formattedDate = dateFormat.format(date);
-        return formattedDate;
-    }
-
-	public static String[] ballColor = { // colors 
-        "Red",
-        "Orange",
-        "Yellow",
-        "Green",
-        "Blue",
-        "Violet",
-        "Black",
-        "White",
-        "Brown",
-        "Cyan",
-    };
-
-	public static String[] ballSomething = { // what is john doing? what will happen tomorrow? what am i eating? 
-    	"Something epic", 
-    	"Nothing of necessity", 
-    	"Your mother", 
-        "Something important"
+	public static String[] ballSomething = { //what is john doing? what will happen tomorrow? what am i eating? 
+    	"Something epic.", 
+    	"Nothing of importance.", 
+    	"Your mother.", 
+    	"Something otherworldly."
   	};
 
-	public static String[] ballPlaces = { // where...?
-		"China",
-		"Area 51",
-		"A planet in another dimension",
-		"Llanfairpwllgwyngyll, a village in Anglesey, Wales"
+	public static String[] ballPlaces = { //where...?
+		"China.",
+		"Area 51.",
+		"A planet in another dimension.",
+		"Llanfairpwllgwyngyll, a village in Anglesey, Wales."
 	};
 
-	public static String[] ballTime = { // when...?
-		"Tomorrow, at exactly 4:20pm",
-		"In approximately 604,800 seconds",
-		"During the winter",
-		"Right now",
-		"Never"
+	public static String[] ballTime = { //when...?
+		"Tomorrow, at exactly 4:20pm.",
+		"In approximately 604,800 seconds.",
+		"During the winter.",
+		"Right now.",
+		"Never."
 	};
 
-	public static String[] ballReasons = { // why...?
-		"Because that's just how it works",
-		"Why not",
-		"No reason in particular"
+	public static String[] ballReasons = { //why...?
+		"Because that's just how it works.",
+		"Why not?",
+		"No reason in particular."
 	};
 
-	// also note that both ballWhoIs and ballWhoAre should be able to answer the question "who ate my sandwich"
+	//also note that both ballWhoIs and ballWhoAre should be able to answer the question "who ate my sandwich"
 
 	public static String[] ballWhoIs = { //who is...?
-		"Someone you know",
-		"Your mother",
-		"Barack Obama",
-		"One that exists only in the imagination",
-		"Someone of slight significance",
-		"John Cena"
+		"Someone you know.",
+		"Your mother.",
+		"Barack Obama.",
+		"One that exists only in the imagination.",
+		"Someone of slight significance.",
+		"John Cena."
 	};
 
-	public static String[] ballWhoAre = { // who are...?
-		"A group in which each individual shares similar traits to other members of the group",
-		"The Second Coming",
-		"Some of your friends"
+	public static String[] ballWhoAre = { //who are...?
+		"A group in which each individual shares similar traits to other members of the group.",
+		"The Second Coming.",
+		"Some of your friends."
 	}; 
 
-	public static String[] ballHow = { // how is john feeling? how did i do on the test? how do birds fly?
-		"That's a secret (I am not legally allowed to tell you)",
-		"Very impressively",
-		"Epically"
+	public static String[] ballHow = { //how is john feeling? how did i do on the test? how do birds fly?
+		"That's a secret (I am not legally allowed to tell you).",
+		"Very impressively.",
+		"Epically."
 	};
 
 	public static String[] ballDefault = {
-		"It is certain",
-		"Without a doubt",
-		"Yes - definitely",
-		"Yep",
-		"Most likely",
-		"Signs point to yes",
-		"I am unsure",
-		"Perhaps",
-		"You do not need to know",
-		"Ask your mother",
-		"Nope",
-		"Most likely not",
-		"Certainly... not",
-		"Probably not",
-		"Definitely not",
-		"No"
-    };
+		"It is certain.",
+		"Without a doubt.",
+		"Yes - definitely.",
+		"Yep.",
+		"Most likely.",
+		"Signs point to yes.",
+		"I am unsure.",
+		"Perhaps.",
+		"You do not need to know.",
+		"Ask your mother.",
+		"Nope.",
+		"Most likely not.",
+		"Certainly... not.",
+		"Probably not.",
+		"Definitely not.",
+		"No."
+	};
 }
--- QuestionChecker.java
+++ QuestionChecker.java
@@ -2,99 +2,72 @@ public class QuestionChecker
 {
     public static int qChecker(String question) 
     {
-        // not a question
-        if(!Magic8.isSensical(question))
-            return 0;
-        else if(Magic8.getLastChar(question) == ".")
+        //what questions
+        if(question.startsWith("what is your name"))
+           return 0;
+        else if(question.startsWith("what are you") || question.equals("who are you") || question.equals("what is this"))
             return 1;
-
-        // what questions
-        else if(question.startsWith("what is your name"))
-            return 2;
-        else if(question.startsWith("what are you") || question.equals("who are you") || 
-                question.equals("what is this"))
-            return 3;
         else if(question.equals("what am i doing"))
-            return 4;
+            return 2;
         else if(question.startsWith("what are you doing"))
-            return 5;
+            return 3;
         else if(question.equals("what do you do"))
-            return 6;
-        else if(question.startsWith("what do") || question.startsWith("what does") || 
-                question.startsWith("what did") || question.startsWith("what will") || 
-                question.startsWith("what would") || question.startsWith("what wont") || 
-                question.startsWith("what might") || (question.startsWith("what") && 
-                question.contains("doing") && !question.contains("you")))
-            return 7;
-        else if(question.startsWith("what") && (question.contains("color") || question.contains("colour")))
-            return 8;
+            return 4;
         else if(question.startsWith("what is"))
-            return 9;
+            return 5;
         else if(question.startsWith("what are"))
+            return 6;
+        else if(question.startsWith("what do") || question.startsWith("what does") || question.startsWith("what did") || question.startsWith("what will") || question.startsWith("what would") || question.startsWith("what wont") || question.startsWith("what might") || (question.startsWith("what") && question.contains("doing") && !question.contains("you")))
+            return 7;
+
+        //who questions
+        else if(question.startsWith("who is joe")) //xddd funy meme
+            return 8;
+        else if(question.startsWith("who is yuri"))
+            return 9;
+        else if(question.startsWith("who is"))
             return 10;
-        else if(question.equals("what time is it"))
+        else if(question.startsWith("who are"))
             return 11;
-        else if(question.startsWith("what time"))
+        else if(question.startsWith("who"))
             return 12;
 
-        // who questions
-        else if(question.startsWith("who is joe")) // xddd funy meme
+        //how questions
+        else if(question.equals("how are you") || question.equals("how do you do"))
             return 13;
-        else if(question.startsWith("who is yuri"))
+        else if(question.startsWith("howdy") || question.startsWith("hello") || question.startsWith("hi"))
             return 14;
-        else if(question.startsWith("who is"))
+        else if(question.startsWith("how") && (question.contains(" i ") || question.contains("me") || question.contains("myself")))
             return 15;
-        else if(question.startsWith("who are"))
+        else if(question.startsWith("how"))
             return 16;
-        else if(question.startsWith("whose"))
+
+        //where 
+        else if(question.startsWith("where"))
             return 17;
-        else if(question.startsWith("who"))
+
+        //when
+        else if(question.startsWith("when"))
             return 18;
 
-        // how questions
-        else if(question.equals("how are you") || question.equals("how do you do"))
+        //why
+        else if(question.startsWith("why"))
             return 19;
-        else if(question.startsWith("howdy") || question.startsWith("hello") || question.startsWith("hi"))
+
+        //or
+        else if(question.contains(" or ") && !(question.startsWith("which") || question.startsWith("what") || question.startsWith("when") || question.startsWith("where") || question.startsWith("why") || question.startsWith("who") || question.startsWith("are")))
             return 20;
-        else if(question.startsWith("how") && (question.contains(" i ") || question.contains("me") || 
-                question.contains("myself")))
+
+        else if(question.contains(" or ") && (question.startsWith("which") || question.startsWith("what") || question.startsWith("when") || question.startsWith("where") || question.startsWith("why") || question.startsWith("who") || question.startsWith("are")))
             return 21;
-        else if(question.startsWith("how"))
+
+        //ballDefault
+        else if(question.startsWith("is") || question.startsWith("are") || question.startsWith("am") || question.startsWith("will") || question.startsWith("would") || question.startsWith("do") || question.startsWith("does") || question.startsWith("did") || question.startsWith("wont") || question.startsWith("might") || question.startsWith("so") || question.startsWith("should"))
             return 22;
 
-        // where 
-        else if(question.startsWith("where"))
-            return 23;
-
-        // when
-        else if(question.startsWith("when"))
-            return 24;
-
-        // why
-        else if(question.startsWith("why"))
-            return 25;
-
-        // or
-        else if(question.contains(" or ") && !(question.startsWith("which") || question.startsWith("what") || 
-                question.startsWith("when") || question.startsWith("where") || question.startsWith("why") || 
-                question.startsWith("who") || question.startsWith("are")))
-            return 26;
-        else if(question.contains(" or ") && (question.startsWith("which") || question.startsWith("what") || 
-                question.startsWith("when") || question.startsWith("where") || question.startsWith("why") || 
-                question.startsWith("who") || question.startsWith("are")))
-            return 27;
-
-        // ballDefault
-        else if(question.startsWith("is") || question.startsWith("are") || question.startsWith("am") || 
-                question.startsWith("will") || question.startsWith("would") || question.startsWith("do") || 
-                question.startsWith("does") || question.startsWith("did") || question.startsWith("wont") || 
-                question.startsWith("might") || question.startsWith("so") || question.startsWith("should") ||
-                question.startsWith("can"))
-            return 28;
-
-        // stop
+        //stop
         else if(question.equals("stop") || question.equals("x"))
-            return 29;
+            return 23;
 
         else
             return -1;
